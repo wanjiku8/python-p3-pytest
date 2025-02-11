b37fbec696cd665d8c3dceb7354e6a7207522453
wanjiku8 <wanjikuf141@gmail.com>
2025-02-11 13:01:40 -0800

    commit

--- lib/not_none_functions.py
+++ lib/not_none_functions.py
@@ -1,2 +1,5 @@
 def return_not_none():
     return None
+
+def test_not_none():
+    assert some_function() is not None
--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from lib.not_none_functions import return_not_none
 
-from not_none_functions import return_not_none
+
+#from not_none_functions import return_not_none
 
 # def test_return_not_none():
 #     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
